efa482e954b86a648c2ad49c5ea2b468fc391436
Fredy Mateo <fredymateomellado@gmail.com>
2025-02-09 22:16:24 -0500

    Limpieza de historial para eliminar secretos

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 //Importamos las librerias necesarias:
+require('dotenv').config();//Para utilizar las variables de entorno.
 const express = require('express'); // Para crear el servidor de backend.
 const app = express(); //Instancia para crear el servidor y manejara las rutas
-const mostrarUsuariosRuta = require ('./rutas/mostrarUsuariosRuta');
+const PORT = process.env.PORT || 4000;
 
+const mostrarUsuariosRuta = require ('./rutas/mostrarUsuariosRuta');
 
 app.use(express.json());
 
-
 app.use('/mostrarUsuarios', mostrarUsuariosRuta);
 
-
 //Iniciamos el servidor con un puerto especifico.
-app.listen(4000, '0.0.0.0', ()=> {
-    console.log('Servidor <<Comunidad>> ejecutandose en el puerto 4000');
+app.get('/', (req, res) => {
+    res.send('Servidor funcionando 🚀');
+});
+
+app.listen(PORT, () => {
+    console.log('Servidor <<Comunidad>> ejecutandose en el puerto ${PORT}');
 });
