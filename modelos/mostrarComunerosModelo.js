1285eba6a56834690aaa1d4cc3ccdcab597f160e
Fredy Mateo <fredymateomellado@gmail.com>
2025-03-07 23:59:32 -0500

    Completando datos en consulta y mostrando en frontend

--- modelos/mostrarComunerosModelo.js
+++ modelos/mostrarComunerosModelo.js
@@ -3,7 +3,7 @@ const conexion = require('../configuracion/conexion.js');
 
 const obtenerComuneros = (callback) => {
     //const consulta = 'SELECT id_prov, nombre_prov, ruc_prov, celular_prov, direccion_prov FROM proveedores';
-    const consulta = 'SELECT id_com, dni_com, apellidos_com, nombres_com FROM comuneros';
+    const consulta = 'SELECT id_com, dni_com, apellidos_com, nombres_com, majada_com, carne_com, condicion_com, caserio_com FROM comuneros';
     conexion.query(consulta, (err, resultados) => {
         if (err) {
             return callback(err, null);
@@ -13,3 +13,6 @@ const obtenerComuneros = (callback) => {
 };
 
 module.exports = { obtenerComuneros };
+
+
+majada_com, carne_com, condicion_com, caserio_com
